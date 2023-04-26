6e4f4f2aa4b6acadc03b82ebcbb680dc6177ec37
wangsen <578595193@qq.com>
2023-04-26 09:45:59 +0800

    FEAT: ObjectComponent add ref, v-model, v-bind, v-if support

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+<p align="left">
+  <a href="https://www.npmjs.com/package/vue"><img src="https://img.shields.io/npm/v/vue3-extension.svg?sanitize=true" alt="Version"></a>
+</p>
+
 # vue3-extension
 
 ### Install
@@ -9,7 +13,7 @@ $ npm install vue3-extension -S
 
 ```ts
 import { createVue } from 'vue'
-import { createVueExtension } from 'vue-extension'
+import { createVueExtension } from 'vue3-extension'
 
 import App from './App.vue'
 const app = createApp(App)
@@ -26,20 +30,32 @@ app.mount('#app')
 <script lang="tsx" setup>
 import { ref, vShow } from 'vue'
 
+const divRef = ref(null)
+const inputValue = ref('')
 const objectIs = ref({
   _is: 'div',
+  ref: divRef,
   class: 'content',
-  // onClick: () => alert('hello'),
-  '@click.prevent': () => alert('hello'),
   _children: ['hello', 'world', {
     _is: 'span',
     _directives: [[vShow, true]],
     _children: ['!'],
   }, {
     _is: 'span',
+    // onClick: () => alert('hello'),
+    '@click.prevent': () => alert('hello'),
     _slots: {
       default: () => '😊' // jsx
     }
+  }, {
+    _is: 'el-input',
+    // 'v-model': computed({
+    //   get: () => inputValue.value,
+    //   set: nv => inputValue.value = nv
+    // })
+    'v-if': true,
+    'v-model': inputValue,
+    'v-bind': { style: 'border: 1px solid #f2f2f2' }
   }]
 })
 </script>
@@ -57,6 +73,7 @@ export interface ObjectComponentIs extends Record<string, any> {
   _children?: any[] | Record<string, any>;
   _slots?: Record<string, () => JSX.Element>;
   _render?: () => JSX.Element | JSX.Element[];
+  // v-model | v-bind | v-if
   // ...attrs
 }
 ```
--- src/ObjectComponent/index.ts
+++ src/ObjectComponent/index.ts
@@ -1,5 +1,5 @@
-import type { Component, PropType, DirectiveArguments } from 'vue'
-import { defineComponent, computed, h, getCurrentInstance, useSlots, useAttrs, resolveDynamicComponent, withDirectives, withModifiers } from 'vue'
+import type { Ref, Component, PropType, DirectiveArguments } from 'vue'
+import { defineComponent, computed, h, getCurrentInstance, resolveDynamicComponent, withDirectives, withModifiers, isRef, createCommentVNode } from 'vue'
 const ObjectComponent = defineComponent({
   name: 'ObjectComponent',
   inheritAttrs: false,
@@ -10,23 +10,59 @@ const ObjectComponent = defineComponent({
     }
   },
   setup(props, ctx) {
-    const slots = useSlots()
-    const attrs = useAttrs()
     return {
+      vIf: computed(() => {
+        if (Object.prototype.hasOwnProperty.call(props.is, 'v-if')) {
+          return !!isRef(props.is['v-if']) ? props.is['v-if'].value : props.is['v-if']
+        } 
+        return true
+      }),
       attrsIs: computed(() => {
         const finalAttrs = {}
         for (const key in props.is) {
           if (/^_/.test(key)) {
             continue
-          } else if (/^@/.test(key) && typeof props.is[key] == 'function') {
+          } else if (/^@/.test(key) && typeof props.is[key] === 'function') {
             const eventKey = `on${key.charAt(1).toUpperCase() + key.slice(2)}`
             const [realKey, ...modifiers] = eventKey.split('.')
             Object.assign(finalAttrs, { [realKey]: withModifiers((...args: any[]) => props.is[key].bind(props.is)(...args), modifiers) })
-          } else {
+          } else if (/^ref$/.test(key)) {
+            if (typeof props.is[key] === 'function') {
+              Object.assign(finalAttrs, {[key]: props.is[key].bind(props.is)})
+            } else {
+              Object.assign(finalAttrs, {[key]: (bindRef: Ref) => props.is[key] = bindRef })
+            }
+          } else if (/^v-/.test(key)) {
+            if (/^v-model/.test(key)) {
+              const keys = key.split(':')
+              const realKey = keys.length > 1 ? keys[1] : 'modelValue'
+              Object.assign(finalAttrs, { 
+                [realKey]: isRef(props.is[key]) ? props.is[key].value : props.is[key],
+                [`onUpdate:${realKey}`]: (nv: any) => {
+                  if (isRef(props.is[key])) {
+                    props.is[key].value = nv  
+                  } else {
+                    props.is[key] = nv
+                  }
+                }
+              })
+            } else if (/^v-bind/.test(key)) {
+              if (/^v-bind:/.test(key)) {
+                const keys = key.split(':')
+                const realKey = keys.length > 1 ? keys[1] : null
+                if (realKey) {
+                  Object.assign(finalAttrs, { [realKey]: props.is[key] })
+                }
+              } else if (typeof props.is[key] === 'object') {
+                Object.assign(finalAttrs, props.is[key])
+              }
+            }
+          }
+          else {
             Object.assign(finalAttrs, { [key]: props.is[key] })
           }
         }
-        return Object.assign(finalAttrs, attrs)
+        return Object.assign(finalAttrs, ctx.attrs)
       }),
       slotsIs: computed(() => {
         const children = props.is._children instanceof Array ? { default: props.is._children } : props.is._children
@@ -39,27 +75,27 @@ const ObjectComponent = defineComponent({
             Object.assign(directSlots, { [slotName]: props.is._slots[slotName].bind(props.is) })
           }
         }
-        return { ...childrenSlots, ...directSlots, ...slots }
+        return { ...childrenSlots, ...directSlots, ...ctx.slots }
       })
     }
   },
   render() {
     const instance = getCurrentInstance()
     if (!!this.is._render) {
-      return withDirectives(
+      return this.vIf ? withDirectives(
         h(this.is._render.bind(this.is)()),
         this.is._directives || []
-      )
+      ) : createCommentVNode(`v-if ObjectComponent`, true)
     } else {
       // component
       const component = typeof this.is._is === 'string' ? instance!.appContext.components[this.is._is] || resolveDynamicComponent(this.is._is) : (
         typeof this.is._is === 'function' ? (this.is._is as any).bind(this.is)() : this.is._is
       )
       const attrs = this.is === component ? null : this.attrsIs
-      return withDirectives(
+      return this.vIf ? withDirectives(
         h(component, attrs, this.slotsIs),
         this.is?._directives || []
-      )
+      ) : createCommentVNode(`v-if ObjectComponent`, true)
     }
   }
 })
